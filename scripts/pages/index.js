1ed38ad09c324c78651737eaa7154cbbb49ba5e6
Chaira <chaira10@hotmail.fr>
2023-06-07 18:38:29 +0200

    home page

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,47 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+async function getPhotographers() {
+    try {
+        // Cette ligne utilise la fonction fetch pour envoyer une requête HTTP GET au fichier JSON des photographes
+        const response = await fetch("../../data/photographers.json");
+
+        // Cette ligne utilise la méthode json() pour extraire les données JSON de la réponse HTTP
+        const data = await response.json();
+
+        // Cette ligne retourne un objet contenant les photographes extraits du fichier JSON
+        return { photographers: data.photographers };
+    } catch (error) {
+        // En cas d'erreur lors de la requête ou de l'extraction des données JSON, cette ligne affiche l'erreur dans la console
+        console.error(error);
     }
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+async function displayData(photographers) {
+    // Cette ligne utilise la méthode querySelector pour sélectionner l'élément HTML ayant la classe "photographer_section"
+    const photographersSection = document.querySelector(".photographer_section");
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
+    // Cette ligne utilise la méthode forEach pour itérer sur chaque élément du tableau des photographes
+    photographers.forEach((photographer) => {
+        // Cette ligne utilise une fonction appelée photographerFactory pour créer un modèle de photographe à partir des données du photographe actuel
+        const photographerModel = photographerFactory(photographer);
+
+        // Cette ligne utilise la méthode getUserCardDOM() du modèle de photographe pour obtenir l'élément DOM de la carte utilisateur
+        const userCardDOM = photographerModel.getUserCardDOM();
+
+        // Cette ligne ajoute l'élément DOM de la carte utilisateur à la section des photographes
+        photographersSection.appendChild(userCardDOM);
+    });
+}
+
+
+async function init() {
+    // Récupère les données des photographes en appelant la fonction asynchrone getPhotographers()
+    const { photographers } = await getPhotographers();
+
+    // Appelle la fonction displayData() pour afficher les données des photographes
+    displayData(photographers);
+}
+
+// Appelle la fonction init() pour initialiser l'affichage des données des photographes
+init();
+
     
